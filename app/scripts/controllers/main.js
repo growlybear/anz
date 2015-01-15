04ccd419d96f41d9a90e2ba00606ee8f8e643416
Michael Allan <michaelallan@optushome.com.au>
2015-01-15 19:00:27 +1100

    Recenter, and remove leaflet-directive duplication

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,43 +7,36 @@
  * # MainCtrl
  * Controller of the angularLeafletApp
  */
-angular.module('angularLeafletApp', [
-  'leaflet-directive'
-])
-.controller('MainCtrl', ['$scope', 'earthquakeService', function ($scope, earthquakeService) {
-  angular.extend($scope, {
-    pacificRim: {
-      lat: 18.0,
-      lng: 200.0,
-      zoom: 3
-    },
-    hyderabad: {
-      lat: 25.31,
-      lng: 78.38,
-      zoom: 3
-    },
-    markers: {},
-    quakesCount: 0
-  });
-
-  earthquakeService.getData()
-    .then(function (data) {
-      var temp = {};
-
-      $scope.quakesCount = data.count;
-
-      // Iterate over the JSON returned from remote API and format for markers
-      data.earthquakes.forEach(function (datum) {
-        temp[datum.eqid] = {
-          lat: parseFloat(datum.lat, 10),
-          lng: parseFloat(datum.lon, 10),
-          message: 'Region: ' + datum.region + '<br/>Magnitude: ' + datum.magnitude,
-          focus: false,
-          draggable: false
-        };
-      });
-
-      // Update the earthquake models (once only, for performance) to place markers on the map
-      $scope.markers = temp;
+angular.module('angularLeafletApp')
+  .controller('MainCtrl', ['$scope', 'earthquakeService', function ($scope, earthquakeService) {
+    angular.extend($scope, {
+      hyderabad: {
+        lat: 15.31,
+        lng: 78.38,
+        zoom: 3
+      },
+      markers: {},
+      quakesCount: 0
     });
-}]);
+
+    earthquakeService.getData()
+      .then(function (data) {
+        var temp = {};
+
+        $scope.quakesCount = data.count;
+
+        // Iterate over the JSON returned from remote API and format for markers
+        data.earthquakes.forEach(function (datum) {
+          temp[datum.eqid] = {
+            lat: parseFloat(datum.lat, 10),
+            lng: parseFloat(datum.lon, 10),
+            message: 'Region: ' + datum.region + '<br/>Magnitude: ' + datum.magnitude,
+            focus: false,
+            draggable: false
+          };
+        });
+
+        // Update the earthquake models (once only, for performance) to place markers on the map
+        $scope.markers = temp;
+      });
+  }]);
