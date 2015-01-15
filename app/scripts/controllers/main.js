ac0c18d0d598b7228453e510e0af7605f1e89cae
Michael Allan <michaelallan@optushome.com.au>
2015-01-15 17:02:25 +1100

    Place markers on map, and recenter for nicer initial display

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,16 +16,34 @@ angular.module('angularLeafletApp', [
       lat: 18.0,
       lng: 200.0,
       zoom: 3
-    }
+    },
+    hyderabad: {
+      lat: 25.31,
+      lng: 78.38,
+      zoom: 3
+    },
+    markers: {},
+    quakesCount: 0
   });
 
   earthquakeService.getData()
     .then(function (data) {
-      $scope.earthquakes = data;
+      var temp = {};
 
-      // FIXME Proof of concept for successful x-domain request
-      console.log($scope.earthquakes);
+      $scope.quakesCount = data.count;
+
+      // Iterate over the JSON returned from remote API and format for markers
+      data.earthquakes.forEach(function (datum) {
+        temp[datum.eqid] = {
+          lat: parseFloat(datum.lat, 10),
+          lng: parseFloat(datum.lon, 10),
+          message: 'Region: ' + datum.region + ', Magnitude: ' + datum.magnitude,
+          focus: false,
+          draggable: false
+        };
+      });
+
+      // Update the earthquake models (once only, for performance) to place markers on the map
+      $scope.markers = temp;
     });
-
-
 }]);
